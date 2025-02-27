4683cffd3425b86c5e11afea7d8e2ca25feb1a21
Arthur <aduarte98@outlook.com>
2025-02-27 15:27:15 -0300

    Iniciando o servidor

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string);
+        console.log("MongoDB conectado com sucesso!");
+    } catch (error) {
+        console.log("Erro ao conectar ao MongoDB", error);
+        process.exit(1)
+    }
+};
+
+export default connectDB;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/database";
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
